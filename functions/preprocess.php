9537322d8da7a1e24ef26794542bbe96771dd896
Parkle Lee <parkle.lee@xenyo.com>
2022-11-30 10:56:10 +0800

    Fix preprocess hook

--- functions/preprocess.php
+++ functions/preprocess.php
@@ -6,8 +6,8 @@ use Drupal\Core\Template\AttributeHelper;
  * Implements hook_preprocess().
  */
 function uni_theme_preprocess(&$variables, $hook) {
-  $attributes = ['class' => $variables['suggestions']];
-  $variables['attributes'] = $variables['attributes']
-    ? AttributeHelper::mergeCollections($variables['attributes'], $attributes)
-    : $attributes;
+  $variables['attributes'] = AttributeHelper::mergeCollections(
+    $variables['attributes'] ?? [],
+    [ 'class' => $variables['suggestions'] ],
+  );
 }
