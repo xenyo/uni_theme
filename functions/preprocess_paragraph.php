2266dfb5fc2f2511c2d669d6e5b4689b834095a8
Parkle Lee <parkle.lee@xenyo.com>
2022-12-06 13:17:13 +0800

    Fix paragraph classes

--- functions/preprocess_paragraph.php
+++ functions/preprocess_paragraph.php
@@ -5,9 +5,8 @@
  */
 function uni_theme_preprocess_paragraph(&$variables) {
   $paragraph = $variables['paragraph'];
-  if ($paragraph->hasField('uni_html_class')) {
-    $field = $paragraph->get('uni_html_class');
-    $terms = $field->referencedEntities();
+  if ($paragraph->hasField('uni_classes')) {
+    $terms = $paragraph->uni_classes->referencedEntities();
     foreach ($terms as $term) {
       $variables['attributes']['class'][] = $term->getName();
     }
