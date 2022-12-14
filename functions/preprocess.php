d7d304c8ebb4a27c624f6c5008955a479cacbd4d
Parkle Lee <parkle.lee@xenyo.com>
2022-12-14 18:54:19 +0800

    Convert uni modal to pattern

--- functions/preprocess.php
+++ functions/preprocess.php
@@ -1,5 +1,6 @@
 <?php
 
+use Drupal\Component\Utility\Html;
 use Drupal\Core\Template\AttributeHelper;
 
 /**
@@ -10,4 +11,13 @@ function uni_theme_preprocess(&$variables, $hook) {
     $variables['attributes'] ?? [],
     [ 'class' => $variables['suggestions'] ],
   );
+
+  if (substr($hook, 0, 8) === 'pattern_') {
+    $id = Html::cleanCssIdentifier(substr($hook, 8));
+    $variant = Html::cleanCssIdentifier($variables['variant']);
+    $variables['attributes'] = AttributeHelper::mergeCollections(
+      $variables['attributes'] ?? [],
+      [ 'class' => [ $id, "$id--$variant"] ],
+    );
+  }
 }
--- templates/pattern/pattern-uni-modal/pattern-uni-modal.js
+++ templates/pattern/pattern-uni-modal/pattern-uni-modal.js
@@ -1,2 +1,2 @@
 (()=>{const e="uni_theme.uni-modal";Drupal.behaviors[e]={attach:(o,a)=>{once(e,".uni-modal").forEach((e=>{e.querySelectorAll("[data-uni-modal-close]").forEach((o=>{o.addEventListener("click",(()=>{e.classList.remove("is-open"),document.body.classList.remove("overflow-hidden")}))}))}))}}})();
-//# sourceMappingURL=uni-modal.js.map
+//# sourceMappingURL=pattern-uni-modal.js.map
