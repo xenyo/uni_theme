d2c9c93648636fb6c69352ed7cbd8a2e2530a71e
Parkle Lee <parkle.lee@xenyo.com>
2022-12-28 11:41:17 +0800

    Fix section classes

--- functions/preprocess_paragraph.php
+++ functions/preprocess_paragraph.php
@@ -5,10 +5,8 @@
  */
 function uni_theme_preprocess_paragraph(&$variables) {
   $paragraph = $variables['paragraph'];
-  if ($paragraph->hasField('uni_classes')) {
-    $terms = $paragraph->uni_classes->referencedEntities();
-    foreach ($terms as $term) {
-      $variables['attributes']['class'][] = $term->getName();
-    }
+  if ($paragraph->hasField('uni_section_vertical')) {
+    $class = $paragraph->get('uni_section_vertical');
+    $variables['attributes']['class'][] = $class->getString();
   }
 }
